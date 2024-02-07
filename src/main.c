82518cb29fc2511ab4433a780b4c09503eba1cd8
Conan Bitter <conan.bitter@gmail.com>
2024-02-07 20:59:31 +0300

    Implement getting line number

--- src/main.c
+++ src/main.c
@@ -1,11 +1,41 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
-#define INPUT_BUFFER_SIZE 5
+#define INPUT_BUFFER_SIZE 256
 
 char input_buffer[INPUT_BUFFER_SIZE];
 
-void read_input() {
+bool is_number(char symbol) {
+    return symbol >= '0' && symbol <= '9';
+}
+
+bool is_space(char symbol) {
+    return symbol == ' ' || symbol == '\t';
+}
+
+char* caret;
+
+int read_int() {
+    int result = 0;
+    while (*caret != '\0' && is_number(*caret)) {
+        result = result * 10 + *caret - '0';
+        caret++;
+    }
+    return result;
+}
+
+bool assert_and_skip_spaces() {
+    if (!is_space(*caret)) {
+        return 1;
+    }
+    while (*caret != '\0' && is_space(*caret)) {
+        caret++;
+    }
+    return 0;
+}
+
+void get_input() {
     while (1) {
         printf("> ");
         fflush(stdout);
@@ -22,17 +52,33 @@ void read_input() {
             int ch;
             while (((ch = getchar()) != '\n') && (ch != EOF)) {
             }
-            input_buffer[INPUT_BUFFER_SIZE - 1] = '\n';
+            input_buffer[INPUT_BUFFER_SIZE - 1] = '\0';
+            return;
         }
-        break;
+
+        input_buffer[strlen(input_buffer) - 1] = '\0';
+        return;
     }
+}
+
+void process_input() {
+    caret = input_buffer;
+    if (is_number(input_buffer[0])) {
+        int line_num = read_int();
+        printf("number %d\n", line_num);
+        if (assert_and_skip_spaces() == 1) {
+            printf("Syntax error: no spaces after line number\n");
+            return;
+        }
+    }
+    printf(": \"%s\"\n", caret);
 }
 
 int main() {
     printf("Hello world!\n");
     while (1) {
-        read_input();
-        printf(": %s\n", input_buffer);
+        get_input();
+        process_input();
     }
     return 0;
 }
